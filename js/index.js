ce265704265bf72a0ff5e9e77b34f0a47d34377d
Hanna <116320862+queirozhanna@users.noreply.github.com>
2023-05-23 18:39:42 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function calculateTip(event){
+    event.preventDefault();
+    let bill = document.getElementById("bill").value;
+    let serviceQuality = document.getElementById("serviceQuality").value;
+    let number = document.getElementById("people").value;
+    if(bill == '' | serviceQuality == 0){
+        alert("Por favor, preencha os valores")
+        return;
+    }
+    if(number == '' | number <= 1){
+        number = 1;
+        document.getElementById("each").style.display = "none"
+    } else {
+            document.getElementById("each").style.display = "block"
+        }
+        let total = (bill * serviceQuality) / number
+        total = total.toFixed(2);
+        document.getElementById("tip").innerHTML = total;
+        document.getElementById("totalTip").style.display = "block";
+    }
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+document.getElementById("tipsForm").addEventListener('submit', calculateTip);
